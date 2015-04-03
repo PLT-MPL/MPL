4f6ca049b783c6cc52d49e573d210b0798322a85
YourName <yt2443@columbia.edu>
2015-04-03 17:34:54 -0400

    commit

--- MPL/src/definition/Melody.java
+++ MPL/src/definition/Melody.java
@@ -5,23 +5,36 @@ public class Melody {
 	
 	private List<Note> m_noteList;
 	
-	public Melody()
-	{
+	public Melody(){
+		
 		m_noteList = new ArrayList<Note>();
 	}
 	
-	public Melody(List<Note> noteList)
-	{
+	public Melody(List<Note> noteList){
+		
 		m_noteList = new ArrayList<Note>(noteList);
 	}
 	
-	public Melody(Melody melody)
-	{
+	public Melody(Melody melody){
 		
+		if(m_noteList == null)
+			m_noteList = new ArrayList<Note>();
+		
+		m_noteList = melody.getNoteList();
 	}
 	
-	public Melody subMelody(int startPoint, int endPoint)
-	{
+	public List<Note> getNoteList(){
+		
+		return m_noteList;
+	}
+	
+	public void setNoteList(List<Note> noteList){
+		
+		m_noteList = noteList;
+	}
+	
+	public Melody subMelody(int startPoint, int endPoint){
+		
 		List<Note> newNoteList = new ArrayList<Note>();
 		
 		if((startPoint < 0) || (endPoint >= m_noteList.size()))
@@ -35,23 +48,47 @@ public class Melody {
 		return subMelody;
 	}
 	
-	public void addNote(Note note)
-	{
+	public void addNote(Note note){
+		
 		if(m_noteList == null)
 			m_noteList = new ArrayList<Note>();
 		
 		m_noteList.add(note);
 	}
 	
-	public void deleteNote(int index)
-	{
+	public void deleteNote(int index){
+		
 		if(index >= m_noteList.size())
 			throw new NullPointerException(); 
 		
 		m_noteList.remove(index);
 	}
 	
+	public Note getNote(int index){
+		
+		return m_noteList.get(index);
+	}
+	
+	public List<Note> getNotes(){
+		
+		return m_noteList;
+	}
+	
 	public int getLength(){
+		
 		return m_noteList.size();
 	}
+	
+	public int getTimeLength(){
+		
+		int maxEnd = 0;
+		for(Note note : m_noteList)
+		{
+			int endtime = note.getStarttime() + note.getDuration();
+			if(endtime > maxEnd)
+				maxEnd = endtime;
+		}
+		return maxEnd;
+	}
 }
+
