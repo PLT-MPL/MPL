4b97edddeccfc35a9240d5c3279086acc5ad16dc
Yilin Xiong <yx2274@columbia.edu>
2015-04-17 17:33:43 -0400

    Remove irrelated files

--- helloMain.ml
+++
@@ -1,5 +0,0 @@
-let _ =
-  let lexbuf = Lexing.from_channel stdin in
-    let result = HelloParser.program HelloLexer.token lexbuf in
-      print_string ("----OUT----\n"^result); print_newline(); flush stdout
-;;
