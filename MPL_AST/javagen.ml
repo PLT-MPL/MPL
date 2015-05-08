16e61ccc8a02bffd93aacb47400d0da2d7ad5aa5
wangbo49 <wangbo49@yeah.net>
2015-05-08 13:51:29 -0400

    javacode generation2

--- MPL_AST/javagen.ml
+++ MPL_AST/javagen.ml
@@ -18,7 +18,6 @@ let rec string_of_expr = function
 	| Dot_Expr(e,s) -> string_of_expr e ^ "." ^ s
 	| Self_Add(e) -> string_of_expr e ^ "++"
 	| Self_Sub(e) -> string_of_expr e ^ "--"
-
 	| Self_Add2(e) -> "++" ^ string_of_expr e
 	| Self_Sub2(e) -> "--" ^ string_of_expr e
 	| Uminus(e) -> "-" ^ string_of_expr e
@@ -28,11 +27,11 @@ let rec string_of_expr = function
 		let typeE1 = check_expr env e1 in
 		let typeE2 = check_expr env e2 in
 		match (typeE1,typeE2) with
-		| ("Melody","Melody") -> "multipleMelody("^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
-		| ("Music","Music") -> "multipleMusic("^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
-		| ("Melody","int") -> "multipleInt"^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
-		| ("int","Melody") -> "multipleInt"^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
-		|  -> "(" ^ string_of_expr e1 ^ ")" ^ "*" ^ "(" ^ string_of_expr e2 ^ ")"
+		| ("Melody","Melody") -> "PublicFunction.multipleMelody("^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
+		| ("Music","Music") -> "PublicFunction.multipleMusic("^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
+		| ("Melody","int") -> "PublicFunction.multipleInt"^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
+		| ("int","Melody") -> "PublicFunction.multipleInt"^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
+		| _-> "(" ^ string_of_expr e1 ^ ")" ^ "*" ^ "(" ^ string_of_expr e2 ^ ")"
 	)
 
 	
@@ -41,14 +40,14 @@ let rec string_of_expr = function
 		let typeE1 = check_expr env e1 in
 		let typeE2 = check_expr env e2 in
 		match (typeE1,typeE2) with
-		| ("Melody","Melody") -> "addMelody(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
-		| ("Music","Music") ->	"addMusic(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")" 
-		| ("Note","Note") -> "addNote("^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
-		| ("Note","int") -> "plus(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")" 
-		| ("int","Note") -> "plus(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")" 
-		| ("Melody","Note") -> "addNote(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")" 
-		| ("Note", "Meldoy") -> "addNote(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
-		| -> string_of_expr e1 ^ "+" ^ string_of_expr e2
+		| ("Melody","Melody") -> "PublicFunction.addMelody(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
+		| ("Music","Music") ->	"PublicFunction.addMusic(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")" 
+		| ("Note","Note") -> "PublicFunction.addNote("^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
+		| ("Note","int") -> "PublicFunction.plus(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")" 
+		| ("int","Note") -> "PublicFunction.plus(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")" 
+		| ("Melody","Note") -> "PublicFunction.addNote(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")" 
+		| ("Note", "Meldoy") -> "PublicFunction.addNote(" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
+		| _-> string_of_expr e1 ^ "+" ^ string_of_expr e2
 	)
 	
 	
@@ -56,8 +55,8 @@ let rec string_of_expr = function
 		let typeE1 = check_expr env e1 in
 		let typeE2 = check_expr env e2 in
 		match (typeE1,typeE2) with
-		| ("Note","int") -> "minus" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
-		| -> string_of_expr e1 ^ "-" ^ string_of_expr e2
+		| ("Note","int") -> "PublicFunction.minus" ^ string_of_expr e1 ^ "," ^ string_of_expr e2^")"
+		| _-> string_of_expr e1 ^ "-" ^ string_of_expr e2
 	)
 	
 
@@ -73,16 +72,33 @@ let rec string_of_expr = function
 
 	| Or(e1,e2) -> string_of_expr e1 ^ "||" ^ string_of_expr e2
 
-	| Expr(e1,e2,e3) -> string_of_expr e1 ^ " " ^ string_of_ass_op e2 ^ " " ^ string_of_expr e3 (*a b c*)
+	| Expr(e1,e2,e3) -> (
+		match e2 with 
+			| "+=" -> 
+				let typeE1 = check_expr env e1 in
+				let typeE2 = check_expr env e2 in
+				if (typeE1  = "Melody" && typeE2 = "Melody") then string_of_expr e1 ^ "." ^ "addMelody(" ^ e3 ^ ")"
+				else if (typeE1 = "Music" && typeE2 = "Music") then string_of_expr e1 ^ "." ^ "addMusic(" ^ e3 ^ ")"
+				else if (typeE1 = "Note" && typeE2 = "Note") then string_of_expr e1 ^ "." ^ "addNote(" ^ e3 ^ ")"
+				else if (typeE1 = "Note" && typeE2 = "int") then string_of_expr e1 ^ "." ^ "plus(" ^ e3 ^ ")"
+				else if (typeE1 = "Melody" && typeE2 = "Note") then string_of_expr e1 ^ "." ^ "addNote(" ^ e3 ^ ")"
+				else string_of_expr e1 ^ " " ^ "+=" ^ " " ^ string_of_expr e3 (*a b c*)
+			| "-=" ->
+				let typeE1 = check_expr env e1 in
+				let typeE2 = check_expr env e2 in
+				if(typeE1 = "Note" && typeE2 = "int") then string_of_expr e1 ^ "." ^ "minus(" ^ e3 ^ ")"
+				else  string_of_expr e1 ^ " " ^ "-=" ^ " " ^ string_of_expr e3
+
+			| "*=" ->
+				let typeE1 = check_expr env e1 in
+				let typeE2 = check_expr env e2 in
+				if(typeE1  = "Melody" && typeE2 = "Melody") then string_of_expr e1 ^ "." ^ "multipleMelody(" ^ e3 ^ ")"
+				else if(typeE1  = "Music" && typeE2 = "Music") then string_of_expr e1 ^ "." ^ "multipleMusic(" ^ e3 ^ ")"
+				else if(typeE1  = "Melody" && typeE2 = "int") then string_of_expr e1 ^ "." ^ "multipleInt(" ^ e3 ^ ")"
+				else string_of_expr e1 ^ " " ^ "*=" ^ " " ^ string_of_expr e3
+	)
 	| Paren_Expr(e1) -> string_of_expr e1
-	| Array(e1,e2) -> 
-
-
-
-
-
-
-	string_of_expr e1 ^ "[" ^ string_of_expr e2 ^ "]"(* a[4] *)
+	| Array(e1,e2) -> string_of_expr e1 ^ "[" ^ string_of_expr e2 ^ "]"(* a[4] *)
 	| Func_Call(e1,Some(a)) -> (match e1 with
 		| "print" -> "PublicFunction.println(" ^ String.concat ", " (List.map string_of_expr a)^")"
 		| "read" -> "PublicFunction.read(" ^ String.concat ", " (List.map string_of_expr a)^ ")"
@@ -124,7 +140,7 @@ let rec string_of_init = function
 		match  typeE with
 		| "Array_Track" -> "new ArrayList<Track>(Arrays.asList("^ String.concat "," (List.map string_of_init e)^"))"
 		| "Array_Note" -> "new ArrayList<Note>(Arrays.asList("^ String.concat "," (List.map string_of_init e)^"))"
-		|  ->"{" ^ String.concat "," (List.map string_of_init e) ^ "}" 
+		| _->"{" ^ String.concat "," (List.map string_of_init e) ^ "}" 
 )
 
 let rec string_of_dec_list = function
@@ -145,7 +161,7 @@ let rec string_of_stmt = function
 		match  typed with 
 		| "Array_Track"-> "List<Track>" ^ string_of_dec_list d 
 		| "Array_Note" -> "List<Note>" ^ string_of_dec_list d 
-		| -> datatype ^ " " ^ string_of_dec_list d ^ ";" 
+		| _-> datatype ^ " " ^ string_of_dec_list d ^ ";" 
 
 	)
 	| If(e,stmt) -> "if " ^ "(" ^ string_of_expr e ^ ")" ^ string_of_stmt stmt
