de36abd91a04a34eeacdaba37216fc357cc9c69b
Ying Tan <yt2443@columbia.edu>
2015-05-11 00:05:43 -0400

    insertMElody

--- MPL_JAVA/MPL/src/definition/Track.java
+++ MPL_JAVA/MPL/src/definition/Track.java
@@ -33,8 +33,8 @@ public class Track {
 		
 		if(startTime < 0)
 			throw new IllegalArgumentException();
-		
-		for(int i = 0; i < melody.getLength(); i++){
+		int len = melody.getLength();
+		for(int i = 0; i < len; i++){
 			Note note = melody.getNote(i);
 			long tmp_startTime = note.getStartTime() + startTime;
 			Note newNote = new Note(note);
